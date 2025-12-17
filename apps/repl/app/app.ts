c203c3083909c907590157f1b420ff9a17f7e866
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-12-17 12:10:59 -0500

    I think it has something to do with the edit template

--- apps/repl/app/app.ts
+++ apps/repl/app/app.ts
@@ -26,7 +26,7 @@ export default class App extends Application {
   inspector = setupInspector(this);
   modules = {
     './router': Router,
-    ...import.meta.glob('./templates/{application,edit,output,error-404}.gts', {
+    ...import.meta.glob('./templates/{application,edit}.gts', {
       eager: true,
     }),
 
